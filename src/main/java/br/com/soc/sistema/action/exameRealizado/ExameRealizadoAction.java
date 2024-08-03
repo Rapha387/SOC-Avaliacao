7d965641b61166458aadea6b1f11c09d47775185
Rapha387 <raphalima@gmail.com>
2024-08-03 19:38:33 -0300

    adicionado paginas e mapeamento de exames realizados pelos funcionarios

---
+++ src/main/java/br/com/soc/sistema/action/exameRealizado/ExameRealizadoAction.java
@@ -0,0 +1,12 @@
+package br.com.soc.sistema.action.exameRealizado;
+
+import br.com.soc.sistema.infra.Action;
+
+public class ExameRealizadoAction extends Action{
+
+	public String todos() {
+		
+		
+		return SUCCESS;
+	}	
+}
